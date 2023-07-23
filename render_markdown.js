073dc42af3eea89d0ac018726054cb8e39ca1ab9
Radu Benea <kitanatahu@gmail.com>
2023-07-23 03:17:58 +0300

    fix rendering with headers, styling, and icon

--- render_markdown.js
+++ render_markdown.js
@@ -6,13 +6,28 @@ marked.use({
 });
 
 export async function render_markdown(context, key) {
-  const value = await context.env.BLOG.get(key);
+  var value = await context.env.BLOG.get(key);
 
   if (value === null) {
-    return new Response("Value not found", { status: 404 });
+    value = "<h1>Adică știi când țiuie?</h1>";//return new Response("Page not found", { status: 404 });
   }
-  var response = new Response(
-    marked.parse(value),
+  var response = new Response(`
+<!DOCTYPE html>
+<html lang="en">
+<head>
+<meta charset="utf-8">
+<link rel="icon" href="/favicon.png">
+<link rel="stylesheet" href="/style.css">
+<meta name="viewport" content="width=device-width,initial-scale=1">
+<title>Art of Feeling</title>
+</head>
+<body>
+<main>
+` + marked.parse(value) + `
+</main>
+</body>
+</html>
+`,
     { headers: { "Content-Type": "text/html" } }
   );
   return response;
